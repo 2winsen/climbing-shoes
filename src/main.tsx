635261c428795320444bb0f51b0d63f9ddcdbf02
2winsen <vitalijs.shakels@gmail.com>
2023-07-26 11:29:27 +0300

    version

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import packageJson from '../package.json';
 import App from './App';
 import './index.scss';
 import './variables.scss';
+
+console.log('******************************');
+console.log(`version: ${packageJson.version}`);
+console.log('******************************');
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
