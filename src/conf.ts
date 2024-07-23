8411a1d6f93e90fd604df93b7894a1181a1e4fc4
2winsen <vitalijs.shakels@gmail.com>
2024-07-23 17:03:17 +0300

    https

--- src/conf.ts
+++ src/conf.ts
@@ -1,4 +1,4 @@
 export const USE_MOCKS = false;
 export const VERBOSE_LOGGING = true;
 export const MAX_WAIT = 90;
-export const CORS_PROXY_URL = 'http://*REMOVED*:8080';
+export const CORS_PROXY_URL = 'https://*REMOVED*:443';
